0cc42baa4f99d4c856de7f73d21537cb347aca37
Kyle Brown <kyle.r.brown@hotmail.co.uk>
2018-07-20 21:47:45 +0100

    Committing post method which checks if the request is valid by checking for the raml extension.

--- src/main/java/com/brown/kyle/api/controller/ApiController.java
+++ src/main/java/com/brown/kyle/api/controller/ApiController.java
@@ -1,9 +1,16 @@
 package com.brown.kyle.api.controller;
 
+import java.io.File;
 import java.util.Calendar;
 
+import org.hibernate.validator.internal.constraintvalidators.hv.ISBNValidator;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +30,13 @@ public class ApiController {
 	@Value("${api.version}")
 	private String version;
 
+	/**
+	 * This method essentially acts as a health check to ensure the api is alive.
+	 * Hello world.
+	 * 
+	 * @return Information Object
+	 */
+
 	@GetMapping("/info")
 	@ResponseBody
 	public Information getApiInformation() {
@@ -37,7 +51,28 @@ public class ApiController {
 		return info;
 
 	}
-	
-//TODO: Create a post method which encrypts data..
+
+	/**
+	 * 
+	 * This method will check whether the requests fields are valid, the user will
+	 * be met with an error
+	 * 
+	 * @param file
+	 * @return
+	 * @return boolean
+	 */
+
+	@PostMapping
+	public ResponseEntity<?> isInformationValid(@RequestBody File file) {
+
+		boolean isValid = false;
+
+		if (file.getAbsolutePath().endsWith("*.raml")) {
+			isValid = true;
+			return new ResponseEntity<>(isValid, HttpStatus.OK);
+		}
+		return new ResponseEntity<>(isValid, HttpStatus.BAD_REQUEST);
+
+	}
 
 }
